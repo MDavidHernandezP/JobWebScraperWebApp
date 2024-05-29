0bd662e3d1973984dd50c45bb408d1cbe1a1e644
MDavidHernandezP <mdavidhernandezp@gmail.com>
2024-05-29 11:24:48 -0600

    Many changes in the app.py principal script, in the main.html I added the functionality in the querys and results sections, also I removed some useless sections of the nav, also for this, I created the respective htmls for the two other pages (about us and acknowledgement) but they have basically nothing inside, and that's it.

--- app.py
+++ app.py
@@ -1,22 +1,32 @@
-from flask import Flask, render_template, url_for, redirect
+from flask import Flask, render_template, url_for, redirect, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def main():
-    return render_template("main.html")
+    return render_template("welcome.html")
 
 @app.route("/welcome")
 def welcome():
     return render_template("welcome.html")
 
-@app.route("/home")
+@app.route("/home", methods=["POST", "GET"])
 def home():
-    return render_template("main.html")
+    if request.method == "POST":
+        selected_options = request.form.getlist("options")
+        print(selected_options)
 
-@app.route("/info")
-def info():
-    return render_template("info.html")
+        return render_template("main.html", results=selected_options)
+    
+    return render_template("main.html", results=None)
+
+@app.route("/aboutus")
+def aboutus():
+    return render_template("aboutus.html")
+
+@app.route("/acknow")
+def acknow():
+    return render_template("acknow.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
